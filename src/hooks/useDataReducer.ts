dc22238fc54a8b922bc3f085bbaf5d398be26fb6
kiku-hub <tacyskani@gmail.com>
2024-11-11 20:06:26 +0900

    useReducerで状態管理をするモジュールを完成

--- src/hooks/useDataReducer.ts
+++ src/hooks/useDataReducer.ts
@@ -4,6 +4,7 @@ import { GenreType } from "../interfaces/GenreType";
 
 export type dataAction = {
   type: "tasksUpdate" | "genresUpdate";
+  payload: { task?: TaskType[]; genre?: GenreType[] };
 };
 
 export type Data = {
@@ -11,14 +12,34 @@ export type Data = {
   genresData: GenreType[];
 };
 
-export const useDataReducer = (): any => {
-  const initialData = {
-    tasksData: [],
-    genresData: [],
+export const useDataReducer = (): [Data, ({ type, payload }: dataAction) => void] => {
+  const initialData: Data = {
+    tasksData: [
+      {
+        id: 0,
+        name: "",
+        explanation: "",
+        deadlineDate: "",
+        status: 0,
+        genreId: 0,
+      },
+    ],
+    genresData: [{ id: 0, name: "" }],
   };
 
   const reducer = (state: Data, action: dataAction) => {
-    return state;
+    switch (action.type) {
+      case "tasksUpdate":
+        return {
+          ...state,
+          tasksData: action.payload.task || state.tasksData,
+        };
+      case "genresUpdate":
+        return {
+          ...state,
+          genresData: action.payload.genre || state.genresData,
+        };
+    }
   };
 
   const [data, dispatch] = useReducer(reducer, initialData);
