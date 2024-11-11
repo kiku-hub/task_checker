0ae01f5b8862c5abe775f5419c787dad4854ea98
kiku-hub <tacyskani@gmail.com>
2024-11-11 15:14:23 +0900

    ジャンル選択部分のマークアップ

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,19 @@
-import React from "react";
 import { Header } from "../../components/header";
+import { Select } from "../../components/select";
+import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
 import "./style.css";
 
 export const Home = () => {
   return (
     <div className="main">
-      <Header></Header>
+      <Header />
+      <div className="genre">
+        <Select />
+        <AddCircleOutlineIcon
+          className="add_circle_outline_icon"
+          fontSize="medium"
+        />
+      </div>
     </div>
   );
 };
